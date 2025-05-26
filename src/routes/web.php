1afaa7c73d34d817b68f73eb0998900a09eb6774
Hideyuki-T <hide27yuki@gmail.com>
2025-05-27 07:30:16 +0900

    [fix]

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,11 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::get('/ping', function () {
     return response()->json(['status' => 'ok'], 200);
 });
 
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
