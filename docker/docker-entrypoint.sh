32d14232ef544c3385d2b4de4a039e9c6da77465
Hideyuki-T <hide27yuki@gmail.com>
2025-05-30 00:36:01 +0900

    [feat]すべてのステージ・スクリプト・Compose 定義を /var/www に統一

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 set -e
 
+cd /var/www
+
+# キャッシュクリア＆キー設定
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-if [ ! -f .env ] && [ -f .env.example ]; then
+if [ ! -f .env ]; then
   cp .env.example .env
   php artisan key:generate --force
 fi
 
-
-# 起動順序
+# PHP-FPM と Nginx を起動
 php-fpm -D
 nginx -g "daemon off;"
-
-# ログを流す
-tail -f /var/log/nginx/error.log
