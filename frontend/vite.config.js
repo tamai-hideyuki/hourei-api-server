572ccc3592a9ddde578cc3de999f61d72ab4366c
Hideyuki-T <hide27yuki@gmail.com>
2025-05-30 15:45:52 +0900

    [feat]

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,17 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    // 既存のプロキシ設定
+    allowedHosts: ['hideyukionline.net'],
+    host: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://api:8080',
         changeOrigin: true,
       },
     },
-
-    allowedHosts: [
-      'hideyukionline.net',
-
-    ]
   }
 })
